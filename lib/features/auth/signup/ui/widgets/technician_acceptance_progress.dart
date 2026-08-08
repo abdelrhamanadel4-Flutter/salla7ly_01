260a388559683de5c402233ce236c12f7407dc0f
Abdelrhman Elsayed <ba590138@gmail.com>
2026-08-09 00:15:33 +0300

    -add waiting request screen

--- lib/features/auth/signup/ui/widgets/technician_acceptance_progress.dart
+++ lib/features/auth/signup/ui/widgets/technician_acceptance_progress.dart
@@ -8,28 +8,23 @@ import 'package:salla7ly/core/theming/assets.dart';
 class TechnicianAcceptanceProgress extends StatelessWidget {
   TechnicianAcceptanceProgress({super.key});
 
-  double progress = 0.25;
-  Timer? timer;
-
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Stack(
         alignment: Alignment.center,
         children: [
-          Image.asset(Assets.imagesLogo),
           SizedBox(
             width: 220.w,
             height: 220.h,
             child: CircularProgressIndicator(
-              value: progress,
               strokeWidth: 24,
               backgroundColor: AppColors.progressIndicatorColor,
               color: AppColors.primaryColor,
             ),
           ),
 
-          Image.asset(Assets.imagesLogo, width: 120, height: 120),
+          Image.asset(Assets.imagesLogo),
         ],
       ),
     );
