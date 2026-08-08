23391ccdf829ba65f23d588bda3267ecf959945c
Abdelrhman Elsayed <ba590138@gmail.com>
2026-08-09 00:03:00 +0300

    -loop circle progress indicator and edit text in TechnicianAcceptanceScreen

--- lib/features/auth/signup/ui/widgets/technician_acceptance_progress.dart
+++ lib/features/auth/signup/ui/widgets/technician_acceptance_progress.dart
@@ -1,15 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:salla7ly/core/theming/app_color.dart';
 import 'package:salla7ly/core/theming/assets.dart';
 
 class TechnicianAcceptanceProgress extends StatelessWidget {
-  TechnicianAcceptanceProgress({super.key});
-
-  double progress = 0.25;
-  Timer? timer;
+  const TechnicianAcceptanceProgress({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +12,17 @@ class TechnicianAcceptanceProgress extends StatelessWidget {
       child: Stack(
         alignment: Alignment.center,
         children: [
-          Image.asset(Assets.imagesLogo),
           SizedBox(
             width: 220.w,
             height: 220.h,
             child: CircularProgressIndicator(
-              value: progress,
-              strokeWidth: 24,
+              strokeWidth: 24.w,
               backgroundColor: AppColors.progressIndicatorColor,
               color: AppColors.primaryColor,
             ),
           ),
 
-          Image.asset(Assets.imagesLogo, width: 120, height: 120),
+          Image.asset(Assets.imagesLogo),
         ],
       ),
     );
